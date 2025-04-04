2d8621f999dbe68f25a290c31cf81cac0e0df883
dh4rio <marzchelo1@gmail.com>
2025-04-03 19:26:08 -0500

    Eliminando detalles

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
 import { Button } from '@/components/ui/button';
-import Navbar from './components/Navbar';
-import Logo from '@/components/Shared/Logo';
 
 export default function Home() {
   return (
     <div>
-      <Logo />
       <Button className='font-normal'>Hello world</Button>
-      <Navbar user='hola' />
     </div>
   );
 }
--- components/Shared/Logo/Logo.tsx
+++
@@ -1,5 +0,0 @@
-const Logo = () => {
-  return <div className='p-2 rounded-lg bg-slate-200 w-fit mb-3'>Dhario</div>;
-};
-
-export default Logo;
